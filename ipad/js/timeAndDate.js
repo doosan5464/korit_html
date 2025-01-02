3dafaceccc0ebe371a52b97f645792d591039da2
doosan5464 <doosan5464@gmail.com>
2025-01-03 00:07:54 +0900

    ddd

--- ipad/js/timeAndDate.js
+++ ipad/js/timeAndDate.js
@@ -1,16 +1,24 @@
 function clockRun() {
-    const phoneClock = document.querySelector(".device-header-left-time")
+    const phoneAmPm = document.querySelector(".device-header-left-ampm");
+    const phoneClock = document.querySelector(".device-header-left-time");
+    const phoneDate = document.querySelector(".device-header-left-date");
+
     setInterval(() => { 
         const now = new Date();
         const nowHours = now.getHours();
         const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
         const nowClockText = `${nowHours}:${nowMinutes}`;
-
-        const nowAmPmText = now.getHours() <= 12 ? "AM" : "PM"; 
-
-
-        phoneAmPm.innerHTML = nowAmPmText;
         phoneClock.innerHTML = nowClockText;
+        
+        const nowAmPmText = now.getHours() >= 12 ? "오후" : "오전";
+        phoneAmPm.innerHTML = nowAmPmText;
+
+        const month = now.getMonth() + 1;
+        const date = now.getDate();
+        const dayOfWeekNumber = now.getDay(); 
+        const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+        const dayOfWeekText = daysOfWeek[dayOfWeekNumber];
+        phoneDate.innerHTML = `${month}월 ${date}일 ${dayOfWeekText}요일`;
     }, 1000);
 }
 
